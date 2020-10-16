c71a474a846e2c29f857bf870833ae43148d2cb4
AJIEKCanderG <sanya.spb_vohta@mail.ru>
2020-10-16 09:59:43 +0300

    1.6. Взаимодействие объектов [#353358]

---
+++ src/main/java/ru/job4j/oop/Ball.java
@@ -0,0 +1,10 @@
+package ru.job4j.oop;
+
+public class Ball {
+    public void tryGoAfterHare(Hare hare) {
+    }
+    public void tryGoAfterWolf(Wolf wolf) {
+    }
+    public void tryGoAfterFox(Fox fox) {
+    }
+}
---
+++ src/main/java/ru/job4j/oop/BallStory.java
@@ -0,0 +1,17 @@
+package ru.job4j.oop;
+
+public class BallStory {
+    public static void main(String[] args) {
+        Ball ball = new Ball();
+        Hare hare = new Hare();
+        Wolf wolf = new Wolf();
+        Fox fox = new Fox();
+        hare.tryEat(ball);
+        ball.tryGoAfterHare(hare);
+        wolf.tryEat(ball);
+        ball.tryGoAfterWolf(wolf);
+        fox.tryEat(ball);
+        ball.tryGoAfterFox(fox);
+    }
+
+}
