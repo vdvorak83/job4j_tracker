e981043f3edc693447dd0b7073fbdc5ab252b977
AJIEKCanderG <sanya.spb_vohta@mail.ru>
2020-10-19 14:10:34 +0300

    2.2. Рефакторинг - Площадь треугольника. [#353351]

---
+++ src/main/java/ru/job4j/oop/Triangle.java
@@ -0,0 +1,33 @@
+package ru.job4j.oop;
+
+public class Triangle {
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
+
+    public double period(double a, double b, double c) {
+        return (a + b + c) / 2;
+    }
+
+    public boolean exist(double ab, double ac, double bc) {
+        return ab + ac > bc && ac + bc > ab && ab + bc > ac;
+    }
+
+    public double area() {
+        double rsl = 0;
+        double ab = first.distance(second);
+        double ac = first.distance(third);
+        double bc = second.distance(third);
+        double p = period(ab, ac, bc);
+        if (this.exist(ab, ac, bc)) {
+            rsl = Math.sqrt(p * (p - ab) * (p - bc) * (p - ac));
+        }
+        return rsl;
+    }
+}
---
+++ src/test/java/ru/job4j/oop/PointTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.oop;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PointTest {
+
+    @Test
+    public void whenDistanceX1Y1Y2ZeroX2TwoThenTwo() {
+        int expected = 2;
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
+        double out = a.distance(b);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenDistanceX1Y1Y2OneX2TwoThenOne() {
+        int expected = 1;
+        Point a = new Point(1, 1);
+        Point b = new Point(2, 1);
+        double out = a.distance(b);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenDistanceX1Y1Y2TwoX2FourThenTwo() {
+        int expected = 2;
+        Point a = new Point(2, 2);
+        Point b = new Point(4, 2);
+        double out = a.distance(b);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
