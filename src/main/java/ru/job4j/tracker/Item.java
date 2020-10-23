8204865e05c7577192010304c0eec941b69c4f64
AJIEKCanderG <sanya.spb_vohta@mail.ru>
2020-10-23 15:57:14 +0300

    6. Date. Отображение даты. [#353364]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,14 @@
 package ru.job4j.tracker;
+import java.time.LocalDateTime;
 
 public class Item {
     private int id;
     private String name;
+    private final LocalDateTime created = LocalDateTime.now();
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
 
     public int getId() {
         return id;
---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,14 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item item = new Item();
+        final LocalDateTime created = item.getCreated();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String createdFormat = created.format(formatter);
+        System.out.println("Текущие дата и время: " + createdFormat);
+    }
+}
