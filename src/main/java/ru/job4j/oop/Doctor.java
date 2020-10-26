ae9d1d7484eb41e3b4136aa9dd22fb671c2ec703
AJIEKCanderG <sanya.spb_vohta@mail.ru>
2020-10-26 14:23:15 +0300

    3. Реализация профессий в коде [#353373]

---
+++ src/main/java/ru/job4j/oop/Doctor.java
@@ -0,0 +1,10 @@
+package ru.job4j.oop;
+
+public class Doctor extends Profession {
+    private Diagnosis heal;
+    private Pacient pacient;
+
+    public Diagnosis heal(Pacient pacient) {
+         return heal;
+    }
+}
---
+++ src/main/java/ru/job4j/oop/Pacient.java
@@ -0,0 +1,5 @@
+package ru.job4j.oop;
+
+public class Pacient extends Doctor {
+
+}
