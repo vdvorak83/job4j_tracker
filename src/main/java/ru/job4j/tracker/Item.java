a1824f596ec1c84960b9aeb80d34ba7d387d5e50
Ajiekcander <sanya.spb_vohta@mail.ru>
2020-10-26 23:22:07 +0300

    7. toString [#353371]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -39,4 +39,12 @@ public class Item {
         this.name = name;
     }
 
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", created=" + created +
+                '}';
+    }
 }
--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,10 +5,12 @@ import java.time.format.DateTimeFormatter;
 
 public class StartUI {
     public static void main(String[] args) {
-        Item item = new Item();
+        Item item = new Item("admin", 1);
+        System.out.println(item);
         final LocalDateTime created = item.getCreated();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String createdFormat = created.format(formatter);
         System.out.println("Текущие дата и время: " + createdFormat);
     }
+
 }
