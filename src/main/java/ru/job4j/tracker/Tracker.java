fd59a7f157e69de91b6efdd70b5788c6bcb9d756
AJIEKCanderG <sanya.spb_vohta@mail.ru>
2020-10-28 14:44:02 +0300

    6. Метод замены заявки. Tracker.replace [#353379]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -31,15 +31,32 @@ public class Tracker {
         return itemsName;
     }
 
-    public Item findById(int id) {
-        Item rsl = null;
+    private int indexOf(int id) {
+        int rsl = -1;
         for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
+            if (items[index].getId() == id) {
+                rsl = index;
                 break;
             }
         }
         return rsl;
+    }
+
+    public Item findById(int id) {
+        /* Находим индекс */
+        int index = indexOf(id);
+        /* Если индекс найден возвращаем item, иначе null */
+        return index != -1 ? items[index] : null;
+    }
+
+    public boolean replace(int id, Item item) {
+        int index = indexOf(id);
+        item.setId(id);
+        items[index] = item;
+        if (index == -1) {
+            return false;
+        } else {
+            return true;
+        }
     }
 }
