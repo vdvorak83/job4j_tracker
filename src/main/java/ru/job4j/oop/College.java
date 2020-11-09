7167dc406311eb7bc5c03266f1900da96f8e5119
Ajiekcander <sanya.spb_vohta@mail.ru>
2020-11-09 23:55:56 +0300

    3.1. Приведение типов. Повышение типа и понижение типа. [#353308]

---
+++ src/main/java/ru/job4j/oop/College.java
@@ -0,0 +1,12 @@
+package ru.job4j.oop;
+
+public class College {
+    public static void main(String[] args) {
+        Freshman freshman = new Freshman();
+
+        Student fm = freshman;
+
+        Object obj = freshman;
+
+    }
+}
