005141692581adeccc4e744d4aa17d761207bbb2
AJIEKCanderG <sanya.spb_vohta@mail.ru>
2020-11-11 14:03:53 +0300

    5.1. Тестирование. Подготовка данных. [#353312]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -33,7 +33,7 @@ public class StartUITest {
         Tracker tracker = new Tracker();
         Item item = new Item("new item");
         tracker.add(item);
-        String[] answers = {String.valueOf(item.getId()), "deleted item"};
+        String[] answers = {String.valueOf(item.getId())};
         StartUI.deleteItem(new StubInput(answers), tracker);
         Item deleted = tracker.findById(item.getId());
         assertNull(deleted);
