9ba698f007086c6267a74cf46d6ad423e0bd4074
Ajiekcander <sanya.spb_vohta@mail.ru>
2020-11-15 01:24:08 +0300

    0.2. Кидаем исключение - throw new RuntimeException [#353323]

---
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -0,0 +1,17 @@
+package ru.job4j.ex;
+
+public class Fact {
+    public static void main(String[] args) {
+    }
+
+    public int calc(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("N less then 0.");
+        }
+        int rsl = 1;
+        for (int index = 1; index <= n; index++) {
+            rsl *= index;
+        }
+        return rsl;
+    }
+}
