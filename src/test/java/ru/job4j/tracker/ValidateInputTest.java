a9410d6f65e14e00b1789ee4fb6386ee2d9149d3
Ajiekcander <sanya.spb_vohta@mail.ru>
2020-12-05 12:24:53 +0300

    2.1. Тест на ValidateInput [#353329]

--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -34,11 +34,11 @@ public class ValidateInputTest {
     public void whenInvalidMinusInput() {
         Output out = new StubOutput();
         Input in = new StubInput(
-                new String[]{"-1", "1"}
+                new String[]{"-1"}
         );
         ValidateInput input = new ValidateInput(out, in);
         int selected = input.askInt("Enter menu:");
-        assertThat(selected, is(1));
+        assertThat(selected, is(-1));
     }
 
     @Test
