02ca282904332f614e99c5bf1e7c8f3168f92ed3
AJIEKCanderG <sanya.spb_vohta@mail.ru>
2021-02-01 16:03:43 +0300

    2. Очередь с приоритетом на LinkedList [#353283]

---
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -0,0 +1,23 @@
+package ru.job4j.search;
+
+import java.util.LinkedList;
+
+public class PriorityQueue {
+    private final LinkedList<Task> tasks = new LinkedList<>();
+
+    public void put(Task task) {
+        int index = 0;
+        for (Task element : tasks) {
+            if (task.getPriority() > element.getPriority()) {
+                index++;
+            } else {
+                break;
+            }
+        }
+        this.tasks.add(index, task);
+    }
+
+    public Task take() {
+        return tasks.poll();
+    }
+}
---
+++ src/main/java/ru/job4j/search/Task.java
@@ -0,0 +1,19 @@
+package ru.job4j.search;
+
+public class Task {
+    private final String desc;
+    private final int priority;
+
+    public Task(String desc, int priority) {
+        this.desc = desc;
+        this.priority = priority;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+}
