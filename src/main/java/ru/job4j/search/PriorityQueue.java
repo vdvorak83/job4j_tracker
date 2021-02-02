25e95416ba828e8b58457bb06db534d5ea445481
AJIEKCanderG <sanya.spb_vohta@mail.ru>
2021-02-02 11:13:31 +0300

    2. Refactor Очередь с приоритетом на LinkedList [#353283]

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -8,11 +8,10 @@ public class PriorityQueue {
     public void put(Task task) {
         int index = 0;
         for (Task element : tasks) {
-            if (task.getPriority() > element.getPriority()) {
-                index++;
-            } else {
-                break;
+            if (task.getPriority() < element.getPriority()) {
+            break;
             }
+         index++;
         }
         this.tasks.add(index, task);
     }
