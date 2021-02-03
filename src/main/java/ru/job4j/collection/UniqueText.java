12469ced7963e94de1902434094a5a281906dec1
AJIEKCanderG <sanya.spb_vohta@mail.ru>
2021-02-03 16:34:28 +0300

    2. Идентичные тексты [#353296]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,24 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+    boolean rsl = true;
+    String[] origin = originText.split(" ");
+    String[] text = duplicateText.split(" ");
+    HashSet<String> check = new HashSet<>();
+      for (String o : origin) {
+          check.add(o);
+      }
+      for (String t : text) {
+          t.contains(originText);
+
+      }
+
+
+    /* for-each origin -> new HashSet. */
+    /* for-each text -> hashSet.contains */
+    return rsl;
+}
+}
