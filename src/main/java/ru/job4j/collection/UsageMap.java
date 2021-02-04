2707c45285660254591a514ddc91b8586ba8a56f
AJIEKCanderG <sanya.spb_vohta@mail.ru>
2021-02-04 14:11:14 +0300

    0. Map, HashMap [#353290]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("ajiekcander@yandex.ru", "Grigorev Aleksandr Andreevich");
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
+
