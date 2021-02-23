e7feb1dc66b030e9fba0a3c3a7ace81598167fb3
Ajiekcander <sanya.spb_vohta@mail.ru>
2021-02-23 17:26:10 +0300

    2. Отсортировать департаменты [#353272]

---
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -0,0 +1,30 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class Departments {
+
+    public static List<String> fillGaps(List<String> deps) {
+        Set<String> tmp = new LinkedHashSet<>();
+        for (String value : deps) {
+            String start = "";
+            for (String el : value.split("/")) {
+                if(start.equals("")) {
+                    start = el;
+                } else {
+                    start += "/" + el;
+                }
+                tmp.add(start);
+            }
+        }
+        return new ArrayList<>(tmp);
+    }
+
+    public static void sortAsc(List<String> orgs) {
+        orgs.sort(Comparator.naturalOrder());
+    }
+
+    public static void sortDesc(List<String> orgs) {
+        orgs.sort(new DepDescComp());
+    }
+}
